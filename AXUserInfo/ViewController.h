94dbc4d8bf049a3ee75986df9da2e61cc743b88a
GG-beyond <872608550@qq.com>
2017-09-04 10:48:59 +0800

    第一次提交

---
+++ AXUserInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXUserInfo
+//
+//  Created by anxindeli on 2017/9/4.
+//  Copyright © 2017年 anxindeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AXUserInfo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AXUserInfo
+//
+//  Created by anxindeli on 2017/9/4.
+//  Copyright © 2017年 anxindeli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
