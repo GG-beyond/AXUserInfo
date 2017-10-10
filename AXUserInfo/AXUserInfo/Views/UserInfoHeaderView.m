99a30d22e38d3a0c63b66f50dd44f9464786fa76
GG-beyond <872608550@qq.com>
2017-10-10 11:38:02 +0800

    删除代码

--- AXUserInfo/AXUserInfo/Views/UserInfoHeaderView.m
+++ AXUserInfo/AXUserInfo/Views/UserInfoHeaderView.m
@@ -46,7 +46,7 @@
 - (void)creatSubViews{
     
     //今日收益
-    UILabel *todayEarning = [[UILabel alloc]initWithFrame:CGRectZero];
+    UILabel *todayEarning = [[UILabel alloc]initWithFrame:CGRectMake(20, 79, SCREEN_WIDTH - 40, 72)];
     todayEarning.font = [UIFont systemFontOfSize: 24];
     todayEarning.adjustsFontSizeToFitWidth = YES;
     todayEarning.textAlignment = NSTextAlignmentCenter;
@@ -54,28 +54,16 @@
     self.todayEarning = todayEarning;
     todayEarning.text = @"0.00";
     [self addSubview:todayEarning];
-    [todayEarning makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(79);
-        make.left.equalTo(20);
-        make.width.equalTo(SCREEN_WIDTH - 40);
-        make.height.equalTo(72);
-    }];
     
-    UILabel *todayTitle = [[UILabel alloc]initWithFrame:CGRectZero];
+    UILabel *todayTitle = [[UILabel alloc]initWithFrame:CGRectMake(20, CGRectGetMaxY(todayEarning.frame)+10, SCREEN_WIDTH-40, 18)];
     todayTitle.font = [UIFont systemFontOfSize:15];
     todayTitle.textAlignment = NSTextAlignmentCenter;
     todayTitle.textColor = [UIColor whiteColor];
     todayTitle.text = @"今日收益";
     [self addSubview:todayTitle];
-    [todayTitle makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(todayEarning.bottom).offset(10);
-        make.height.equalTo(18);
-        make.width.equalTo(200);
-        make.centerX.equalTo(self.centerX);
-    }];
     
     //可用余额
-    UILabel *availableMoney = [[UILabel alloc]initWithFrame:CGRectZero];
+    UILabel *availableMoney = [[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(todayTitle.frame)+30, SCREEN_WIDTH/2.0, 20)];
     availableMoney.font = [UIFont systemFontOfSize: 24];
     availableMoney.textAlignment = NSTextAlignmentCenter;
     availableMoney.textColor = [UIColor whiteColor];
@@ -83,125 +71,14 @@
     availableMoney.text = @"0.00";
     self.availableMoney = availableMoney;
     [self addSubview:availableMoney];
-    [availableMoney makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(todayTitle.bottom).offset(30);
-        make.height.equalTo(20);
-        make.left.equalTo(0);
-        make.width.equalTo(SCREEN_WIDTH/2);
-    }];
     
-    UILabel *availableMoneyTitle = [[UILabel alloc]initWithFrame:CGRectZero];
+    UILabel *availableMoneyTitle = [[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(availableMoney.frame)+5, SCREEN_WIDTH/2.0, 20)];
     availableMoneyTitle.font = [UIFont systemFontOfSize:15];
     availableMoneyTitle.textAlignment = NSTextAlignmentCenter;
     availableMoneyTitle.textColor = [UIColor whiteColor];
     availableMoneyTitle.text = @"可用余额（元）";
     [self addSubview:availableMoneyTitle];
-    [availableMoneyTitle makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(availableMoney.bottom).offset(5);
-        make.height.equalTo(20);
-        make.left.equalTo(0);
-        make.width.equalTo(SCREEN_WIDTH/2);
-    }];
     //中间的竖线
-    UIView *centerView = [[UIView alloc]initWithFrame:CGRectZero];
-    centerView.backgroundColor = RGBCOLOR(65, 146, 227);
-    [self addSubview:centerView];
-    [centerView makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(todayTitle.bottom).offset(35);
-        make.left.equalTo(self.centerX);
-        make.height.equalTo(35);
-        make.width.equalTo(1);
-    }];
-    //右边的已到账收益
-    UILabel *earnedMoney = [[UILabel alloc]initWithFrame:CGRectZero];
-    earnedMoney.font = [UIFont systemFontOfSize:24];
-    earnedMoney.textAlignment = NSTextAlignmentCenter;
-    earnedMoney.textColor = [UIColor whiteColor];
-    earnedMoney.adjustsFontSizeToFitWidth = YES;
-    self.earnedMoney = earnedMoney;
-    earnedMoney.text = @"0.00";
-    [self addSubview:earnedMoney];
-    [earnedMoney makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(todayTitle.bottom).offset(30);
-        make.height.equalTo(20);
-        make.left.equalTo(self.centerX);
-        make.right.equalTo(0);
-    }];
-    
-    UILabel *earnedMoneyTitle = [[UILabel alloc]initWithFrame:CGRectZero];
-    earnedMoneyTitle.font = [UIFont systemFontOfSize:15];
-    earnedMoneyTitle.textAlignment = NSTextAlignmentCenter;
-    earnedMoneyTitle.textColor = [UIColor whiteColor];
-    earnedMoneyTitle.text = @"已到账收益（元）";
-    [self addSubview:earnedMoneyTitle];
-    [earnedMoneyTitle makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(availableMoney.bottom).offset(5);
-        make.height.equalTo(20);
-        make.left.equalTo(centerView.right).offset(0);
-        make.right.equalTo(0);
-    }];
-
-    
-    self.enumImageArray = @[@"user_charge.png",@"user_withdraw.png",@"user_ property.png",@"user_coupons.png"];
-    self.enumTitleArray = @[@"充值",@"提现",@"资产",@"优惠券"];
-    for (int i = 0; i < self.enumTitleArray.count ; i++) {
-        UIView *enumView = [[UIView alloc]initWithFrame:CGRectZero];
-        enumView.backgroundColor = [UIColor whiteColor];
-        [self addSubview:enumView];
-        [enumView makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(280);
-            make.left.equalTo(i*SCREEN_WIDTH/4);
-            make.width.equalTo(SCREEN_WIDTH/4);
-            make.height.equalTo(95);
-        }];
-        
-        UIButton *enumSender = [UIButton buttonWithType:UIButtonTypeCustom];
-        [enumView addSubview:enumSender];
-        enumSender.tag = i;
-        [enumSender addTarget:self action:@selector(doSelectEnum:) forControlEvents:UIControlEventTouchUpInside];
-                
-        UIImageView *enumImageView = [[UIImageView alloc]initWithFrame:CGRectZero];
-        enumImageView.image = [UIImage imageNamed:self.enumImageArray[i]];
-        [enumView addSubview:enumImageView];
-        [enumImageView makeConstraints:^(MASConstraintMaker *make) {
-            make.width.equalTo(30);
-            make.height.equalTo(30);
-            make.top.equalTo(20);
-            make.centerX.equalTo(enumView.centerX);
-        }];
-        
-        
-        enumSender.titleLabel.font = [UIFont systemFontOfSize:15];
-        [enumSender setTitleEdgeInsets:UIEdgeInsetsMake(40, 0, 0, 0)];
-        [enumSender setTitle:self.enumTitleArray [i] forState:UIControlStateNormal];
-        [enumSender setTitleColor:RGBCOLOR(38, 42, 59) forState:UIControlStateNormal];
-        [enumSender makeConstraints:^(MASConstraintMaker *make) {
-            make.edges.equalTo(enumView);
-        }];
-        
-        UIView *rightLine = [[UIView alloc] initWithFrame:CGRectZero];
-        rightLine.backgroundColor = RGBCOLOR(207, 220, 230);
-        [enumView addSubview:rightLine];
-        if (i == 3) {
-            rightLine.hidden = YES;
-        }
-        [rightLine makeConstraints:^(MASConstraintMaker *make) {
-            make.width.equalTo(1);
-            make.height.equalTo(95);
-            make.right.equalTo(enumView.right).offset(0);
-            make.top.equalTo(0);
-        }];
-        
-        UIView *bottomLine = [[UIView alloc]initWithFrame:CGRectZero];
-        bottomLine.backgroundColor = RGBCOLOR(207, 220, 230);
-        [enumView addSubview:bottomLine];
-        [bottomLine makeConstraints:^(MASConstraintMaker *make) {
-            make.width.equalTo(SCREEN_WIDTH/4);
-            make.height.equalTo(1);
-            make.bottom.equalTo(enumView.bottom);
-            make.left.equalTo(0);
-        }];
-    }
 }
 //button实现的方法
 - (void)doSelectEnum:(UIButton *)sender{
